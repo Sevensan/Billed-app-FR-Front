cce0b742f56ef0eb1e8f64e1fc0514ccf718d105
Nicolas LEPETIT <nicolas.lepetit@pxcom.aero>
2022-06-21 12:44:33 +0200

    fix bug report bills antichronologic display bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -5,7 +5,8 @@ import LoadingPage from "./LoadingPage.js"
 import Actions from './Actions.js'
 
 const row = (bill) => {
-  return (`
+  console.log('bill', bill)
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,16 +17,17 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
+    `
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const sortByAntiChrono = data.sort((a,b) => new Date(b.date) - new Date(a.date))
+  return data && data.length ? sortByAntiChrono.map(bill => row(bill)).join('') : ''
 }
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  console.log('bills', bills)
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +42,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `
 
   if (loading) {
     return LoadingPage()
   } else if (error) {
     return ErrorPage(error)
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -75,6 +77,6 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
       ${modal()}
-    </div>`
-  )
+    </div>
+  `
 }
